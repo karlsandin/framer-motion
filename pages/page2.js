c0994cc675d00e8a075680524b600d45e8350e02
Karl Sandin <karlsandin83@gmail.com>
2021-11-19 16:31:22 +0100

    feat: add basic routing pages

---
+++ pages/page2.js
@@ -0,0 +1,12 @@
+import Header from "components/Header/Header";
+
+const Page2 = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Page 2</h1>
+    </div>
+  );
+};
+
+export default Page2;
---
+++ pages/page3.js
@@ -0,0 +1,12 @@
+import Header from "components/Header/Header";
+
+const Page3 = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Page 3</h1>
+    </div>
+  );
+};
+
+export default Page3;
